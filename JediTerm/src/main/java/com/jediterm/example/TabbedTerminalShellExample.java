1140791aab1a154911474de48759552aa3aa5351
Sebastian Kürten <sebastian@topobyte.de>
2025-06-03 17:46:07 +0200

    Let Ctrl-T create a new terminal tab

--- JediTerm/src/main/java/com/jediterm/example/TabbedTerminalShellExample.java
+++ JediTerm/src/main/java/com/jediterm/example/TabbedTerminalShellExample.java
@@ -61,11 +61,8 @@ public class TabbedTerminalShellExample {
     tabbed = new JTabbedPane();
     frame.setContentPane(tabbed);
 
-    int count = 2;
+    addTerminalWidget(true);
 
-    for (int i = 0; i < count; i++) {
-      addTerminalWidget();
-    }
     frame.addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosing(WindowEvent e) {
@@ -78,9 +75,19 @@ public class TabbedTerminalShellExample {
 
     frame.pack();
     frame.setVisible(true);
+
+    InputMap inputMap = tabbed.getInputMap();
+    ActionMap actionMap = tabbed.getActionMap();
+    inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.CTRL_DOWN_MASK), "ctrl-t");
+    actionMap.put("ctrl-t", new AbstractAction() {
+      @Override
+      public void actionPerformed(ActionEvent actionEvent) {
+        addTerminalWidget(true);
+      }
+    });
   }
 
-  private void addTerminalWidget() {
+  private void addTerminalWidget(boolean focus) {
     JediTermWidget widget = createTerminalWidget();
     widgets.add(widget);
     widget.getTerminal().addApplicationTitleListener(title -> {
@@ -98,15 +105,20 @@ public class TabbedTerminalShellExample {
       });
     });
 
-    InputMap inputMap = widget.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-    ActionMap actionMap = widget.getActionMap();
-    inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.CTRL_DOWN_MASK), "ctrl-t");
-    actionMap.put("ctrl-t", new AbstractAction() {
+    widget.getTerminalPanel().addCustomKeyListener(new KeyAdapter() {
       @Override
-      public void actionPerformed(ActionEvent actionEvent) {
-        addTerminalWidget();
+      public void keyPressed(KeyEvent e) {
+        super.keyTyped(e);
+        if (e.getKeyCode() == KeyEvent.VK_T && e.isControlDown()) {
+          addTerminalWidget(true);
+        }
       }
     });
+
+    if (focus) {
+      tabbed.setSelectedComponent(widget);
+      widget.getTerminalPanel().requestFocus();
+    }
   }
 
   public static void main(String[] args) {
