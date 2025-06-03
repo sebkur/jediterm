115a172a25de713b698289abd2ac21ddaa7d6268
Sebastian Kürten <sebastian@topobyte.de>
2025-06-03 10:04:43 +0200

    Add example with tabbed terminal

---
+++ JediTerm/src/main/java/com/jediterm/example/TabbedTerminalShellExample.java
@@ -0,0 +1,117 @@
+package com.jediterm.example;
+
+import com.jediterm.pty.PtyProcessTtyConnector;
+import com.jediterm.terminal.CursorShape;
+import com.jediterm.terminal.TtyConnector;
+import com.jediterm.terminal.ui.JediTermWidget;
+import com.jediterm.terminal.ui.settings.DefaultSettingsProvider;
+import com.pty4j.PtyProcess;
+import com.pty4j.PtyProcessBuilder;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.jediterm.app.PlatformUtilKt.isWindows;
+
+public class TabbedTerminalShellExample {
+
+  private static @NotNull JediTermWidget createTerminalWidget() {
+    JediTermWidget widget = new JediTermWidget(80, 24, new DefaultSettingsProvider());
+    widget.getTerminalPanel().setDefaultCursorShape(CursorShape.BLINK_UNDERLINE);
+    widget.setTtyConnector(createTtyConnector());
+    widget.start();
+    return widget;
+  }
+
+  private static @NotNull TtyConnector createTtyConnector() {
+    try {
+      Map<String, String> envs = System.getenv();
+      String[] command;
+      if (isWindows()) {
+        command = new String[]{"cmd.exe"};
+      }
+      else {
+        command = new String[]{"/bin/bash", "--login"};
+        envs = new HashMap<>(System.getenv());
+        envs.put("TERM", "xterm-256color");
+      }
+
+      PtyProcess process = new PtyProcessBuilder().setCommand(command).setEnvironment(envs).start();
+      return new PtyProcessTtyConnector(process, StandardCharsets.UTF_8);
+    }
+    catch (Exception e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  private JFrame frame;
+  private JTabbedPane tabbed;
+  private List<JediTermWidget> widgets = new ArrayList<>();
+
+  private void createAndShowGUI() {
+    frame = new JFrame("Basic Terminal Shell Example");
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    tabbed = new JTabbedPane();
+    frame.setContentPane(tabbed);
+
+    int count = 2;
+
+    for (int i = 0; i < count; i++) {
+      addTerminalWidget();
+    }
+    frame.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent e) {
+        frame.setVisible(false);
+        for (JediTermWidget widget : widgets) {
+          widget.getTtyConnector().close(); // terminate the current process
+        }
+      }
+    });
+
+    frame.pack();
+    frame.setVisible(true);
+  }
+
+  private void addTerminalWidget() {
+    JediTermWidget widget = createTerminalWidget();
+    widgets.add(widget);
+    widget.getTerminal().addApplicationTitleListener(title -> {
+      int index = tabbed.indexOfComponent(widget);
+      tabbed.setTitleAt(index, title);
+    });
+    tabbed.add(widget);
+    widget.addListener(terminalWidget -> {
+      widget.close(); // terminate the current process and dispose all allocated resources
+      SwingUtilities.invokeLater(() -> {
+        tabbed.remove(widget);
+        if (tabbed.getTabCount() == 0) {
+          System.exit(0);
+        }
+      });
+    });
+
+    InputMap inputMap = widget.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+    ActionMap actionMap = widget.getActionMap();
+    inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.CTRL_DOWN_MASK), "ctrl-t");
+    actionMap.put("ctrl-t", new AbstractAction() {
+      @Override
+      public void actionPerformed(ActionEvent actionEvent) {
+        addTerminalWidget();
+      }
+    });
+  }
+
+  public static void main(String[] args) {
+    // Create and show this application's GUI in the event-dispatching thread.
+    TabbedTerminalShellExample tabbedTerminal = new TabbedTerminalShellExample();
+    SwingUtilities.invokeLater(tabbedTerminal::createAndShowGUI);
+  }
+}
